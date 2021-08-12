4301532a62907017b603e2994acba2a6f94bc72a
natpacket <1399092738@qq.com>
2021-08-12 12:23:32 +0800

    add:新增微信消息通知功能 :star:

--- autoCheck.py
+++ autoCheck.py
@@ -6,6 +6,56 @@ timeout = 20
 ss = requests.Session()
 
 
+class WXMsg:
+
+    def __init__(self, corpid, secret, agentid):
+        self.corpid = corpid
+        self.secret = secret
+        self.agentid = agentid
+        self.access_token = None
+
+    def get_token(self):
+        access_token = None
+        try:
+            url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+            params = {"corpid": self.corpid, "corpsecret": self.secret}
+            resp = requests.get(url=url, params=params)
+            # print(resp.text)
+            access_token = resp.json().get('access_token')
+            self.access_token = access_token
+            # print(access_token)
+        except Exception as e:
+            print('error:', e)
+        # pass
+        return access_token
+
+    def send_msg(self, title=None, content=None, touser='@all', toparty=None, access_token=None):
+        if access_token is None:
+            self.get_token()
+            print(self.access_token)
+            access_token = self.access_token
+        url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
+        # print(url)
+        payload = {
+            "touser": touser,
+            # "toparty": toparty,
+            # "totag": "TagID1 | TagID2",
+            "msgtype": "textcard",
+            "agentid": self.agentid,
+            "textcard": {
+                "title": title,
+                "description": content,
+                "url": "URL",
+                "btntxt": ""
+            },
+            "enable_id_trans": 0,
+            "enable_duplicate_check": 0,
+            "duplicate_check_interval": 1800
+        }
+        resp = requests.post(url=url, json=payload)
+        print(resp.text)
+
+
 # 登录获取cookie
 def login(url, data):
     # url="https://wangzi.uk/user"
@@ -43,16 +93,21 @@ def check(url, headers):
         res = ss.post(url=url, headers=headers)
         pyObject = json.loads(res.text)
         print(pyObject.get("msg"))
+        return pyObject.get("msg")
     except Exception as e:
         print(e)
         print(e.__traceback__.tb_lineno)
 
 
 def main(a, b):
-    # email = input('邮箱：')
-    # passwd = input('密码：')
     with open(file='./user.json', encoding='utf-8') as f:
         data = json.loads(f.read())
+    corpid = data.get('corpid')
+    secret = data.get('secret')
+    agentid = data.get('agentid')
+    touser = data.get('touser')
+    # email = input('邮箱：')
+    # passwd = input('密码：')
     email = data.get('email')
     passwd = data.get('passwd')
     # 登录
@@ -65,7 +120,9 @@ def main(a, b):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
         "Cookie": cookie
     }
-    check(checkUrl, headers)
+    msg = check(checkUrl, headers)
+    wx = WXMsg(corpid, secret, agentid)
+    wx.send_msg(title='ssr签到结果', content=msg, touser=touser)
 
 
 if __name__ == '__main__':
@@ -85,7 +142,8 @@ if __name__ == '__main__':
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
         "Cookie": cookie
     }
-    check(checkUrl, headers)
+    msg = check(checkUrl, headers)
+
     # data = {"a":"b"}
     # res = ss.post(url='http://httpbin.org/post',data=data,proxies=proxies,verify=False)
     # print(res.text)
